1677049a1ebde930c4f7b95714e93d980abe50e0
Kevin <kevinwillian084@gmail.com>
2022-08-02 14:48:49 -0300

    fix(routes): fix route create blogPost
    fix a bug that id was undefined

--- src/controller/blogPostController.js
+++ src/controller/blogPostController.js
@@ -3,7 +3,7 @@ const blogPostService = require('../service/blogPostService');
 const blogPostController = {
   create: async (req, res, _next) => {
     const { title, content, categoryIds } = req.body;
-    const { id } = req.user.dataValues;
+    const { id } = req.user;
     if (!title || !content || !categoryIds.length) throw new Error('MISSING_FIELDS');
     const newPost = await blogPostService.create({ title, content, userId: id, categoryIds });
     return res.status(201).json(newPost);
